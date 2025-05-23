d04c5bb3aa5a3c6732f820bc89e5f9d6415f958e
meowybiang <biangbiang847@gmail.com>
2025-05-23 10:13:49 +0800

    update lang

--- app/Http/Controllers/ProductController.php
+++ app/Http/Controllers/ProductController.php
@@ -9,9 +9,7 @@ use Illuminate\Support\Facades\Storage;
 
 class ProductController extends Controller
 {
-    /**
-     * Display a listing of the resource.
-     */
+   
     public function index() : View
     {
         return view('products.index', [
@@ -19,17 +17,13 @@ class ProductController extends Controller
         ]);
     }
 
-    /**
-     * Show the form for creating a new resource.
-     */
+    
     public function create() : View
     {
         return view('products.create');
     }
 
-    /**
-     * Store a newly created resource in storage.
-     */
+   
     public function store(StoreProductRequest $request) : RedirectResponse
     {
         $data = $request->validated();
@@ -44,31 +38,24 @@ class ProductController extends Controller
             ->withSuccess('New product is added successfully.');
     }
 
-    /**
-     * Display the specified resource.
-     */
+    
     public function show(Product $product) : View
     {
         return view('products.show', compact('product'));
     }
 
-    /**
-     * Show the form for editing the specified resource.
-     */
+    
     public function edit(Product $product) : View
     {
         return view('products.edit', compact('product'));
     }
 
-    /**
-     * Update the specified resource in storage.
-     */
+    
     public function update(UpdateProductRequest $request, Product $product) : RedirectResponse
     {
         $data = $request->validated();
 
         if ($request->hasFile('image')) {
-            // Delete old image if exists
             if ($product->image) {
                 Storage::disk('public')->delete($product->image);
             }
@@ -82,9 +69,6 @@ class ProductController extends Controller
             ->withSuccess('Product is updated successfully.');
     }
 
-    /**
-     * Remove the specified resource from storage.
-     */
     public function destroy(Product $product) : RedirectResponse
     {
         if ($product->image) {
